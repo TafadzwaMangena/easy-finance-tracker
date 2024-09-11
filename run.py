0dde49c0cbe4723268545493571438d701527a89
Tafadzwa Mangena <mangenatafadzwamgcini@gmail.com>
2024-09-11 17:22:57 +0000

    Wire up APIs and confirm access to google sheet

--- run.py
+++ run.py
@@ -1,3 +1,26 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+from simple_term_menu import TerminalMenu
+from tabulate import tabulate
+from colorama import Fore, Style, init
+
+#To initialize Colorama to be compatible with Windows
+init(autoreset=True)
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('easy_finance_tracker')
+
+info = SHEET.worksheet('info')
+
+data = info.get_all_values()
+
+print(data)
+
